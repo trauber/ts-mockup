d6952471656c61fd16dab15995360308014cdf2a
trauber <rich.traube@gmail.com>
2024-07-20 16:19:56 -0400

    Splash sections and a subpage

--- build.sh
+++ build.sh
@@ -6,7 +6,13 @@ done
 
 for i in pages/*.haml; do
     base=$(basename -s .haml "$i")
-    haml "$i" | sed 's/_/ /g' > pages/$base.html
+    file="$base.html"
+    #haml "$i" | sed 's/_/ /g' > $base.html
+    head -1 "pages/$base.haml" > tmp.haml
+    cat page.haml >> tmp.haml
+    sed -i s/__FILE__/$file/ tmp.haml
+    haml tmp.haml | sed 's/_/ /g' | gpp > $file
+    rm -f tmp.haml
 done
 
 haml index.haml | sed 's/_/ /g' | gpp > index.html
